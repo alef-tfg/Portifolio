706f234a3555040a1cbc694c9d26bb984ba2559a
alef-tfg <alef.tfg@gmail.com>
2024-07-15 16:36:35 -0300

    Add data filter

--- Accidents/AccidentsDashboard.py
+++ Accidents/AccidentsDashboard.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from datetime import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -26,29 +27,43 @@ st.title('Accidents in brazilian roads')
 acidentes = pd.read_parquet('Accidents/data/acidentes.parquet').astype({'tipo_veiculo': 'object'}).set_index('momento')
 
 st.sidebar.header('Filtros')
+args = {'format':"DD/MM/YYYY", 'min_value':datetime(2023, 1, 1), 'max_value':datetime(2023, 12, 31)}
+date_i = st.sidebar.date_input('Data inicial', value=datetime(2023, 1, 1), **args )
+date_f = st.sidebar.date_input('Data final', value=datetime(2023, 12, 31), **args )
+if date_i > date_f:
+    st.sidebar.error('A data final é anterior a inicial')
+    st.stop()
+
 vehicles = st.sidebar.multiselect(
     "Selecione os veículos",
     options=acidentes.tipo_veiculo.unique(),
     default=acidentes.tipo_veiculo.unique()
 )
 
-acidentes = acidentes[acidentes.tipo_veiculo.isin(vehicles)]
+acidentes.data_inversa = pd.to_datetime(acidentes.data_inversa)
+date_i = pd.to_datetime(date_i)
+date_f = pd.to_datetime(date_f)
+
+acidentes = acidentes[(acidentes['data_inversa'] >= date_i) & (acidentes['data_inversa'] <= date_f)]
+acidentes = acidentes[acidentes.tipo_veiculo.isin(vehicles)].copy()
 
 col1, col2, col3, col4 = st.columns(4)
 width, height = 1000, 300
 data = acidentes.resample('d').count().reset_index()
 fig = px.line(data, x = 'momento', y = 'br', labels={'momento': 'Dia', 'br': 'Total acidentes'},  width=width, height=height)
-col1.plotly_chart(fig)
+if(date_f.day-date_i.day)>0:
+    col1.plotly_chart(fig)
 
 
 data = acidentes.groupby('dia_semana').count().reset_index()
 fig = px.line(data, x = 'dia_semana', y = 'br', labels={'dia_semana': 'semana', 'br': 'Total acidentes'},  width=width, height=height)
-col2.plotly_chart(fig)
-
+if(date_f.week-date_i.week)>0 or (date_i.week == 52 and date_f.week==52):
+    col2.plotly_chart(fig)
 
 data = acidentes.groupby(acidentes.index.month).count().reset_index()
 fig = px.line(data, x = 'momento', y = 'br', labels={'momento': 'Mês', 'br': 'Total acidentes'},  width=width, height=height)
-col3.plotly_chart(fig)
+if(date_f.month-date_i.month)>0:
+    col3.plotly_chart(fig)
 
 
 data = acidentes.groupby(acidentes.index.hour).count().reset_index()
@@ -72,7 +87,6 @@ col5.plotly_chart(fig)
 
 gdf =gpd.read_file('Accidents/data/states-br.json')
 m = folium.Map(location=[gdf.geometry.centroid.y.mean()-2, gdf.geometry.centroid.x.mean()-5], zoom_start=3.7)
-col7.write([gdf.geometry.centroid.y.mean(), gdf.geometry.centroid.x.mean()])
 folium.GeoJson(gdf).add_to(m)
 
 # Display the map in Streamlit
